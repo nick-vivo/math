0dcf6b633a26f845d0d4b4cbd98bf486f1a0da22
Nikita Frolkin <fokin-klock@mail.ru>
2023-07-26 22:36:03 +0400

    add: (Point.cc::Point) x(), y()

--- include/Point.cc
+++ include/Point.cc
@@ -1,5 +1,5 @@
-#ifndef __TOOLS_H__
-#define __TOOLS_H__
+#ifndef __POINT_CC__
+#define __POINT_CC__
 #pragma once
 
 namespace math
@@ -29,6 +29,16 @@ public:
     inline void set_row(T new_row) noexcept
     {
         this->_row = new_row;
+    }
+
+    inline T& x() noexcept
+    {
+        return this->_col;
+    }
+
+    inline T& y() noexcept
+    {
+        return this->_row;
     }
 };
 
